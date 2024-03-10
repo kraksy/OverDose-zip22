5687c71705b51863febabbb2aded0d972b0c23e5
kraksy <kraksy8@gmail.com>
2024-03-10 19:38:52 +0100

    first commit

---
+++ media/lua/shared/Reloading/onReload.lua
@@ -0,0 +1,15 @@
+
+
+
+
+
+function brokeReload(x)
+      Break = false;
+
+      if x == true then
+            Break = true;
+      end
+
+      -- on reload there will be chance to broke
+      return Break;
+end
